68a153c7d359f1c5f9ce39b2d490a38f5daca21f
Jacob Hampton <jhampton792@gmail.com>
2022-08-19 18:36:50 -0500

    added next link component instead of native a tag

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,24 +1,32 @@
+import Link from 'next/link';
 import styles from './navbar.module.css';
 
 const NavBar = () => (
   <div className={styles.navbar}>
+
+    {/* Logo Section */}
     <div className={styles.no_mw}>
       <h1 className={styles.m_no_vertical}>Logo</h1>
     </div>
+
+    {/* Name Section */}
     <div className={styles.no_mw}>
       <h1 className={styles.m_no_vertical}>Name</h1>
     </div>
+
+    {/* Links Section */}
     <div className={styles.nav_links}>
-      <a className={styles.link} href='/about'>
-        About
-      </a>
-      <a className={styles.link} href='/contact'>
-        Contact
-      </a>
-      <a className={styles.link} href='/'>
-        Home
-      </a>
+      <span className={styles.link}>
+        <Link href='/about' className={styles.link}>About</Link>
+      </span>
+      <span className={styles.link}>
+        <Link href='/contact'>Contact</Link>
+      </span>
+      <span className={styles.link}>
+        <Link href='/'>Home</Link>
+      </span>
     </div>
+
   </div>
 );
 
